edcc44a00b39071a23f638a31844c6dd188afae2
vernoxy <vernoxy3@gmail.com>
2025-11-26 11:01:35 +0530

    Add slider

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,12 @@ import {
 import Home from "./Components/Home";
 import MaterialList from "./Components/MaterialList";
 import AddMateriall from "./Components/AddMateriall";
+import Sidebar from "./Components/SideBar";
 
 const App = () => {
   return (
     <Router>
+      <Sidebar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/material" element={<MaterialList />} />
