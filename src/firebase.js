9264fa0531a41099077675e570cb039379bef3ff
vernoxy2 <vernoxy2@gmail.com>
2025-12-29 13:46:54 +0530

    fix: change db

--- src/firebase.js
+++ src/firebase.js
@@ -3,18 +3,26 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import {
   getAuth,
-  signInAnonymously,
-  onAuthStateChanged,
 } from "firebase/auth";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCcGW8SAt05QEJPFqfll5aBAh_yaJ7tE5o",
+//   authDomain: "jalaram-33f56.firebaseapp.com",
+//   projectId: "jalaram-33f56",
+//   storageBucket: "jalaram-33f56.appspot.com",
+//   messagingSenderId: "308176661731",
+//   appId: "1:308176661731:web:09ea16b99a5764c707a9d6",
+//   measurementId: "G-HRPDKGM176",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCcGW8SAt05QEJPFqfll5aBAh_yaJ7tE5o",
-  authDomain: "jalaram-33f56.firebaseapp.com",
-  projectId: "jalaram-33f56",
-  storageBucket: "jalaram-33f56.appspot.com",
-  messagingSenderId: "308176661731",
-  appId: "1:308176661731:web:09ea16b99a5764c707a9d6",
-  measurementId: "G-HRPDKGM176",
+  apiKey: "AIzaSyARTpH_U_KLr4Np7I316VmSDjx8HuTUsLY",
+  authDomain: "jalaramnewcopy.firebaseapp.com",
+  projectId: "jalaramnewcopy",
+  storageBucket: "jalaramnewcopy.firebasestorage.app",
+  messagingSenderId: "428903801642",
+  appId: "1:428903801642:web:4b8b10820ae4c0a374f453",
+  measurementId: "G-31WEDL9L2Z"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -24,14 +32,3 @@ export const auth = getAuth(app);
 
 // 🔥 Firestore
 export const db = getFirestore(app);
-
-// 🔥 Anonymous Auto Login
-onAuthStateChanged(auth, (user) => {
-  if (!user) {
-    signInAnonymously(auth)
-      .then(() => console.log("Signed in anonymously"))
-      .catch((err) => console.error("Anonymous login failed:", err));
-  } else {
-    console.log("Already logged in:", user.uid);
-  }
-});
